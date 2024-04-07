362c99bc75523349995e994a1382f2937e210517
aliferoz1234 <m.ali_feroz@hotmail.com>
2024-04-07 23:32:20 +0500

    This is a Simple Currency Converter

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    SR: 3.75,
+    UED: 3.67,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "SR", "UED", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "SR", "UED", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD  base currency
+let convertedAmount = baseAmount * toAmount;
+let roundedNumber = convertedAmount.toFixed(0);
+console.log(roundedNumber);
